45a9f58a9c4bc9a8265efb72042708e339f5dced
张雨 <1273044376@qq.com>
2015-07-07 18:46:37 +0800

    Update Exe02.java

--- day01/Exe02.java
+++ day01/Exe02.java
@@ -3,7 +3,7 @@ package day1;
 public class Exe02 {
    public static void main(String[] args)
    {
-	   char c1='’≈';
+	   char c1='Âº†';
 	   char c2='z';
 	   System.out.println("c1="+c1);
 	   System.out.println("c2="+c2);
